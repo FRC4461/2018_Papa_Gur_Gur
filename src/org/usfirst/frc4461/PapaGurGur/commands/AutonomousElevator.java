aff4bdbd2e7aa71b2bbc0556169146cf17f6dff1
Vince M <vincerosane@gmail.com>
2018-03-10 17:04:17 -0800

    cleanup pt 2

--- src/org/usfirst/frc4461/PapaGurGur/commands/AutonomousElevator.java
+++ src/org/usfirst/frc4461/PapaGurGur/commands/AutonomousElevator.java
@@ -6,48 +6,53 @@ import org.usfirst.frc4461.PapaGurGur.RobotMap;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- *	
+ *
  */
 public class AutonomousElevator extends Command {
-	private static final double COUNTS_PER_REVOLUTION = 610;
-	private double countsToMove;
+    private static final double COUNTS_PER_REVOLUTION = 610;
+    private double countsToMove;
 
-	private double elevateSpeed = 0.2;
-	
-	private boolean isDone = false;
+    private double elevateSpeed = 0.2;
 
-	public AutonomousElevator(double inchesToMove) {
-		countsToMove = inchesToMove;
-		requires(Robot.elevator);
-	}
-	
-	public enum ElevatorPositions{
-		DOWN, SWITCH, SCALE_LOW, SCALE_MIDDLE, SCALE_HIGH
-	}
+    private boolean isDone = false;
 
-	protected void initialize() {
-		Robot.elevator.configElevatorEncoder();
-	}
+    public AutonomousElevator(double inchesToMove) {
+	countsToMove = inchesToMove;
+	requires(Robot.elevator);
+    }
 
-	protected void execute() {
-		int elevatorEncoder = RobotMap.elevatorMotor.getSelectedSensorPosition(0);
-		RobotMap.elevatorMotor.set(elevateSpeed);
-		
-		if (elevatorEncoder >= countsToMove) {
-			Robot.elevator.stopElevator();
-			isDone = true;
-		}
-	}
+    public enum ElevatorPositions {
+	DOWN, SWITCH, SCALE_LOW, SCALE_MIDDLE, SCALE_HIGH
+    }
 
-	protected boolean isFinished() {
-		return isDone;
-	}
+    @Override
+    protected void initialize() {
+	Robot.elevator.configElevatorEncoder();
+    }
 
-	protected void end() {
-		Robot.elevator.stopElevator();
-	}
+    @Override
+    protected void execute() {
+	int elevatorEncoder = RobotMap.elevatorMotor.getSelectedSensorPosition(0);
+	RobotMap.elevatorMotor.set(elevateSpeed);
 
-	protected void interrupted() {
-		end();
+	if (elevatorEncoder >= countsToMove) {
+	    Robot.elevator.stopElevator();
+	    isDone = true;
 	}
+    }
+
+    @Override
+    protected boolean isFinished() {
+	return isDone;
+    }
+
+    @Override
+    protected void end() {
+	Robot.elevator.stopElevator();
+    }
+
+    @Override
+    protected void interrupted() {
+	end();
+    }
 }

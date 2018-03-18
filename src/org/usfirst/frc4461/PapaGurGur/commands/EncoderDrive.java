51ba7628525ad8db1cc89c39048791c7427bd4ad
Vince M <vincerosane@gmail.com>
2018-03-18 15:37:18 -0700

    Added optional integer
    Optional integer to let encoder drive to let it do whatever it wants
    whether to have timeout or not

--- src/org/usfirst/frc4461/PapaGurGur/commands/EncoderDrive.java
+++ src/org/usfirst/frc4461/PapaGurGur/commands/EncoderDrive.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc4461.PapaGurGur.commands;
 
+import java.util.Optional;
+
 import org.usfirst.frc4461.PapaGurGur.Robot;
 import org.usfirst.frc4461.PapaGurGur.RobotMap;
 
@@ -18,7 +20,7 @@ public class EncoderDrive extends Command {
     private final static double RAMP_SPEED = 2;
     private final static double LEFT_SPEED = -0.3;
     private final static double RIGHT_SPEED = -0.38;
-    
+
     private static final int DEFAULT_TIMEOUT = 3;
 
     private int countsToMove;
@@ -34,11 +36,23 @@ public class EncoderDrive extends Command {
     public EncoderDrive(double inchesToMove) {
 	this(inchesToMove, DEFAULT_TIMEOUT);
     }
-    
-    public EncoderDrive(double inchesToMove, int timeOutSec){
+
+    public EncoderDrive(double inchesToMove, int timeOutSec) {
 	requires(Robot.driveBase);
 	countsToMove = (int) (COUNTS_PER_INCH * inchesToMove);
 	setTimeout(timeOutSec);
+    }
+
+    /*
+     * Make factory function and overload them to make code clearer and more
+     * readable
+     */
+    public static EncoderDrive GoForwardInches(double inchesToMove, Optional<Integer> timeOutSec) {
+	return new EncoderDrive(inchesToMove, timeOutSec.orElse(DEFAULT_TIMEOUT));
+    }
+
+    public static EncoderDrive GoBackInches(double inchesToMove, Optional<Integer> timeOutSec) {
+	return new EncoderDrive(-inchesToMove, timeOutSec.orElse(DEFAULT_TIMEOUT));
     }
 
     @Override
@@ -51,10 +65,11 @@ public class EncoderDrive extends Command {
     protected void execute() {
 	int leftEncoder = RobotMap.frontLeft.getSelectedSensorPosition(0);
 	int rightEncoder = RobotMap.frontRight.getSelectedSensorPosition(0);
-	
-	System.out.println("Left: " + " " + leftEncoder + "Right: " + rightEncoder + " " + "Counts To Move: " + countsToMove);
+
+	System.out.println(
+		"Left: " + " " + leftEncoder + "Right: " + rightEncoder + " " + "Counts To Move: " + countsToMove);
 	Robot.driveBase.drive(LEFT_SPEED, RIGHT_SPEED);
-	if(leftEncoder >= countsToMove && -rightEncoder >= countsToMove){
+	if (leftEncoder >= countsToMove && -rightEncoder >= countsToMove) {
 	    Robot.driveBase.stopMotors();
 	    isDone = true;
 	}

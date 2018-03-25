1980bf5bfc8b02ba2892e3f210918d53cd23d33a
Vince M <vincerosane@outlook.com>
2018-03-25 12:40:23 -0700

    THINGS WORK PLEASE BE V CAREFUL
    We can go anywhere on the field and place switch and scale cube yay

--- src/org/usfirst/frc4461/PapaGurGur/commandGroups/RightPosRightScale.java
+++ src/org/usfirst/frc4461/PapaGurGur/commandGroups/RightPosRightScale.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc4461.PapaGurGur.commandGroups;
 
 import org.usfirst.frc4461.PapaGurGur.commands.AutonomousElevator;
+import org.usfirst.frc4461.PapaGurGur.commands.Delay;
 import org.usfirst.frc4461.PapaGurGur.commands.EncoderDrive;
 import org.usfirst.frc4461.PapaGurGur.commands.GyroTurn;
 import org.usfirst.frc4461.PapaGurGur.commands.OpenGripper;
@@ -15,9 +16,10 @@ public class RightPosRightScale extends CommandGroup {
 
 	public RightPosRightScale() {
 		addSequential(new EncoderDrive(264));
-		addParallel(AutonomousElevator.GoUp(85));
+		addParallel(AutonomousElevator.GoUp(101));
 		addSequential(GyroTurn.turnLeft(45));
-		addSequential(new EncoderDrive(4));
+		addSequential(new EncoderDrive(58));
+		addSequential(new Delay(1.5));
 		addSequential(new OpenGripper(2));
 //		addSequential(new EncoderDrive(112));
 //		addSequential(GyroTurn.turnLeft(180));
